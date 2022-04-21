eb2df52394aa5bcee8413e035b0861ebe46a9516
Diego Bortolucci <bortolucci.diego@gmail.com>
2022-04-21 15:00:37 -0300

    Main Commit

---
+++ Mamifero.php
@@ -0,0 +1,29 @@
+<?php
+
+    require_once 'Animal.php';
+
+    class Mamififero extends Animal{
+        private $corPelo;
+
+        public function alimentar(){
+            echo "<p> Mamando </p>";
+        }
+
+        public function emitirSom(){
+            echo "<p> Som de Mamífero </p>";
+        }
+
+        public function locomover(){
+            echo "<p> Correndo </p>";
+        }
+
+        public function getCorPelo(){
+            return $this->corPelo;
+        }
+
+        public function setCorPelo($cp){
+            $this->corPelo = $cp;
+        }
+    }
+
+?>
