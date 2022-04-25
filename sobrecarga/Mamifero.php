89fbf9cbddc40adaa3878013bb3e9a09a1012131
Diego Bortolucci <bortolucci.diego@gmail.com>
2022-04-25 15:46:06 -0300

    pastas

---
+++ sobrecarga/Mamifero.php
@@ -0,0 +1,15 @@
+<?php
+
+    require_once 'Animal.php';
+
+    class Mamifero extends Animal
+    {   
+        protected $corPelo;
+
+        public function emitirSom()
+        {
+            
+        }
+    }
+
+?>
