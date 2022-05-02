b8b823a33a1d46111bd06e193379a605b2befbc2
Diego Bortolucci <bortolucci.diego@gmail.com>
2022-05-02 15:08:48 -0300

    update

--- sobrecarga/home.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    require_once 'Mamifero.php';
-
-    $mamifero = new Mamifero();
-    $mamifero->emitirSom();
-
-?>
--- sobrecarga/index.php
+++ sobrecarga/index.php
@@ -1,8 +1,15 @@
 <?php
 
     require_once 'Mamifero.php';
+    require_once 'Lobo.php';
+    require_once 'Cachorro.php';
 
     $mamifero1 = new Mamifero();
     $mamifero1->emitirSom();
 
+    $lobo = new Lobo();
+    $lobo->emitirSom();
+
+    $cachorro = new Cachorro();
+    $cachorro->emitirSom();
 ?>
